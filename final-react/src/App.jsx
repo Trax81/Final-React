e08fc4fc3613d62076c63ce73765f2b0ca0d3cbe
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-06-18 15:17:13 -0700

    TRavis

--- final-react/src/App.jsx
+++ final-react/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import About from "./pages/About.jsx";
 
 
 
--- final-react/src/pages/About.jsx
+++ final-react/src/pages/About.jsx
@@ -1,5 +1,6 @@
 function About() {
-    return <h1>About</h1>
+    return (<h1>About</h1>
+    )
 }
 
 export default About
