3f65b35a11dc1d90f41ee682747cfb5089b7570d
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-06-23 11:08:50 -0700

    TRavis

--- final-react/src/pages/Home.jsx
+++ final-react/src/pages/Home.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+
 function Home() {
-    return (
-    <h1>Home</h1>
-    )
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.omdbapi.com/?s=${searchValue}&apikey=11a85b25")
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data.slice(0, 6));
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>API Items</h1>
+    console.log(movies);
+      {items.map((item) => (
+        <div key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Home
+{movies && movies.slice(0, 6).map((movie) => (
+  <div key={movie.id}>
+    <h2>{movie.title}</h2>
+    <p>{movie.overview}</p>
+  </div>
+))}
+
+export default Home;
