6c537019c666612e83874e3f6c0134b3948e045e
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-06-24 14:01:59 -0700

    Travis

--- final-react/src/pages/Home.jsx
+++ final-react/src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetch("https://www.omdbapi.com/?s=batman&apikey=https://www.omdbapi.com/?s=${searchValue}&apikey=11a85b25")
+    fetch("https://www.omdbapi.com/?s=avatar&apikey=11a85b25")
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.Search ? data.Search.slice(0, 6) : []);
@@ -13,8 +13,8 @@ function Home() {
   }, []);
 
   return (
-    <div>
-      <h1>Movies</h1>
+    <div class="background">
+      <h1>The first 6 Movies of an API</h1>
 
       {movies.map((movie) => (
         <div key={movie.imdbID}>
