b2850903594ee068afb1724b0bf12c749fdaf5ae
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-07-13 15:14:22 -0700

    Travis

--- final-react/src/pages/About.jsx
+++ final-react/src/pages/About.jsx
@@ -1,4 +1,4 @@
-import Home from "./pages/Home.jsx";
+
 import { Link } from "react-router-dom";
 
 function About() {
--- final-react/src/pages/Contact.jsx
+++ final-react/src/pages/Contact.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+
+
+
+
 function Contact() {
-    return (<h1>Contact</h1>
+    return (
+           <div className="about-page">
+        <Link to="/" className="navbar-btn">
+  Home
+</Link>
+      <h1>
+        Contact Travis
+      </h1>
+      <h2>
+        Phone: 503-453-4999
+        <br>
+        </br>
+        Email:Travantmedia@gmail.com
+
+      </h2>
+    </div>
     )
 }
 
